4cfe1580128bc12a23e0999cef63733eecd29499
NoeN <24297098+noenarcisse@users.noreply.github.com>
2026-07-12 10:28:38 +0200

    Update UpdateReadMe.fsx

--- UpdateReadMe.fsx
+++ UpdateReadMe.fsx
@@ -15,27 +15,23 @@ let header = [
     "Liste des catégories : <br>"
 ]
 
-let  display dirNames=
-    dirNames |> List.iter(fun x -> printfn "%s" x)
-    dirNames
+let tee f x = f x ; x
+let  display dirNames= dirNames |> Seq.iter(printfn "%s")
+let encodeForUrl (text:string) = Uri.EscapeDataString text
 
-let encodeForUrl (text:string) =
-    Uri.EscapeDataString(text)
-
-let toMarkdown (list: string list) =
+let toMarkdown (list: string seq) =
     list
-    |> List.map (fun x -> $"[{x}](https://github.com/noenarcisse/MultiLangStudy/tree/main/{encodeForUrl x})  <br>")
+    |> Seq.map (fun x -> $"[{x}](https://github.com/noenarcisse/MultiLangStudy/tree/main/{encodeForUrl x})<br>")
 
-let write file (list:string list) =
-    let content = list |> toMarkdown
+let write file (list:string seq) =
+    let content = list |> toMarkdown |> Seq.toList
     File.WriteAllLines( file, header @ content)
         
 
 
 Directory.GetCurrentDirectory() 
-    |> Directory.GetDirectories
-    |> Array.toList
-    |> List.map Path.GetFileName
-    |> List.filter (fun discard-> ignoreList |> List.contains discard |> not)
-    |> display 
-    |> write readme
+|> Directory.EnumerateDirectories
+|> Seq.map Path.GetFileName
+|> Seq.filter (fun discard-> ignoreList |> List.contains discard |> not)
+|> tee display 
+|> write readme
