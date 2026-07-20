2653b23f5a0ada51d6a83249adf96d873d5b7d2d
NoeN <24297098+noenarcisse@users.noreply.github.com>
2026-07-20 16:04:11 +0200

    et hop

---
+++ updatereadme.nim
@@ -0,0 +1,28 @@
+import os
+import strformat
+import uri
+
+proc formatMarkdown(dirname : string) : string =
+    &"[{dirname}](https://github.com/noenarcisse/MultiLangStudy/tree/main/{encodeUrl dirname})<br>\n"
+
+var f : File = nil
+let ignored = @[".git", ".vscode", "bin", "obj", "_template"]
+var dirs : seq[string] = @[]
+let readme_file = "README.md"
+let header = """
+# Index
+Liste des catégories : <br>
+"""
+var content : string
+
+for e in walkDir(".") :
+    if e.kind != PathComponent.pcDir : continue
+    var dirname = extractFilename(e.path)
+    if dirname in ignored : continue
+    content&=formatMarkdown dirname
+
+if open(f, readme_file, FileMode.fmWrite) :
+    defer : close f
+    f&=header&"\n"&content #write non trycatch
+else :
+    echo "Outch, opening file failed"
